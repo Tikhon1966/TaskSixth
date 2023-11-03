0fd59c48d551b1f6a6be85ae5031e1b1e490dfe8
Oleg Tikhomirov <ovt1966@gmail.com>
2023-11-03 16:59:27 +0300

    Add tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,32 @@
+
 describe('login page', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
   it('should login with valid email and password', () => {
-    cy.visit('http://localhost:3000')
-
-    cy.contains('Log in').click()
-    cy.get('#mail').type('test@test.com')
-    cy.get('#pass').type('test')
     
-    cy.contains('Submit').click()
-
+    cy.login('test@test.com', 'test')
+    
     cy.contains('Добро пожаловать test@test.com').should('be.visible')
+  })
+  
+  it('should not login with empty mail', () => {
+
+    cy.login(null, 'test')
+
+    cy.get('#mail').then((elements) => {
+      expect(elements[0].checkValidity()).to.be.false
+      expect(elements[0].validationMessage).to.be.eql('Заполните это поле.')
+    })
+  })
+
+  it('should not login with empty password', () => {
+
+    cy.login('test@test.com', null)
+
+    cy.get('#pass').then((elements) => {
+      expect(elements[0].checkValidity()).to.be.false
+      expect(elements[0].validationMessage).to.be.eql('Заполните это поле.')
+    })
   })
 })
